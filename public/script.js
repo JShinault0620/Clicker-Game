8a1733cf9ac58c4ed7504a6a4a661e167db3d42f
JShinault0620 <jshincool@gmail.com>
2022-04-02 11:47:39 -0400

    Created upgrade button system

--- public/script.js
+++ public/script.js
@@ -1,3 +1,23 @@
+class Upgrade {
+    constructor(base, mult, cost, target) {
+        this.base = base
+        this.mult = mult
+        this.cost = cost
+        this.target = target
+    }
+
+    use() {
+        if (this.target.pointsTotal > this.cost) {
+            this.target.pointsTotal -= this.cost
+            this.target.pointsBase += this.base
+            this.target.pointsMultiplier += this.mult
+            this.cost = this.cost * 2
+            this.base = this.base * 1.5
+            this.mult = this.base * 1.5
+        }    
+    }
+}
+
 const clickerGame = {
     buttonElement: document.getElementById('clicker-button'),
     pointsElement: document.getElementById('points-view'),
@@ -5,28 +25,48 @@ const clickerGame = {
     pointsTotal: 0,
 
     pointsMultiplier : 1,
-    pointsBase: 10,
+    pointsBase: 1,
 
-    upgradeButtons: [],
+    upgradeSettings: [
+        {base: 1, mult: 0 , cost: 50},
+        {base: 0, mult: 1.5 , cost: 1000},
+        {base: 2, mult: 0 , cost: 10},
+        {base: 0, mult: 2 , cost: 100}
+    ],
 
-    findUpgrades: function() {
-        for (let i = 0; i < 4; i++) {
+    setupUpgrades: function() {
+        for (let i = 0; i < this.upgradeSettings.length; i ++) {
             if (document.getElementById(`upgrade${i}`)) {
-                this.upgradeButtons.push(document.getElementById(`upgrade${i}`))
+                document.getElementById(`upgrade${i}`).addEventListener('click', (e) => {
+                    if (this.pointsTotal >= this.upgradeSettings[i].cost) {
+                        this.pointsTotal -= this.upgradeSettings[i].cost
+                        this.pointsBase += this.upgradeSettings[i].base
+                        this.pointsMultiplier += this.upgradeSettings[i].mult
+                        this.upgradeSettings[i].cost *= 2
+                        this.upgradeSettings[i].base *= 1.5
+                        this.upgradeSettings[i].mult *= 1.5
+                        this.updateDisplays()
+                    }
+                })
             }
         }
+        this.updateDisplays()
     },
 
-    
+    updateDisplays: function() {
+        for (let i = 0; i < this.upgradeSettings.length; i ++) {
+            if (document.getElementById(`upgrade${i}`)) {
+                document.getElementById(`upgrade${i}`).innerText = `Cost is ${this.upgradeSettings[i].cost}`
+            }
+        }
+        this.pointsElement.innerText = `You have ${this.pointsTotal} points.`
+    },
 
     init: function() {
-        this.findUpgrades()
-        console.log(this.upgradeButtons)
+        this.setupUpgrades()
         this.buttonElement.addEventListener('click', (e) => {
             this.pointsTotal += (this.pointsBase * this.pointsMultiplier)
-            this.pointsMultiplier ++
-            this.pointsBase ++
-            this.pointsElement.innerText = `You have ${this.pointsTotal} points.`
+            this.pointsElement.innerText = `You have ${Math.trunc(this.pointsTotal)} points.`
         })
     }
 }
